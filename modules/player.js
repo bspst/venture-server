17c0f24187e06acc49688ead5bb08bfe31c21898
HizkiFW <hizkifw@gmail.com>
2017-01-26 15:43:54 +0700

    Added comments

--- modules/player.js
+++ modules/player.js
@@ -1,12 +1,30 @@
+/**
+ * Player - 
+ */
 v.player = {};
+
+// Fetches the character model of a player
+// Returns the name of the model on success, false on failure.
 v.player.getModel = function(guid) {
 	
 };
-v.player.setModel = function(guid) {
+
+// Sets the character model of a player
+// Returns true on success, false on failure.
+v.player.setModel = function(guid, modelName) {
 	
 };
 
 v.player.inventory = {};
-v.player.inventory.get = function(guid) {
+
+// Gets the container GUID of a player
+// Returns a container GUID on success, false on failure.
+v.player.inventory.getContainerGUID = function(guid) {
 	
 };
+
+// Sets the container GUID of a player
+// Returns true on success, false on failure.
+v.player.inventory.setContainerGUID = function(guid, containerGUID) {
+	
+}
