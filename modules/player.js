15fae345463aaf6770d4ee1637f3a45b1bf6160f
HizkiFW <hizkifw@gmail.com>
2017-01-27 15:34:15 +0700

    Implemented basic player functions

--- modules/player.js
+++ modules/player.js
@@ -3,6 +3,20 @@
  */
 v.player = {};
 
+// Gets a value from the player's data
+// Returns an object
+v.player.getValue = function(guid, key, callback) {
+	v.db.get().ref("players").child(guid).once("value").then(function(ds) {
+		callback(ds.child(key).val());
+	});
+}
+
+// Fetches the player's name
+// Returns a string
+v.player.getName = function(guid, callback) {
+	v.player.getValue(guid, "name", callback);
+}
+
 // Fetches the character model of a player
 // Returns the name of the model on success, false on failure.
 v.player.getModel = function(guid) {
@@ -19,8 +33,8 @@ v.player.inventory = {};
 
 // Gets the container GUID of a player
 // Returns a container GUID on success, false on failure.
-v.player.inventory.getContainerGUID = function(guid) {
-	
+v.player.inventory.getContainerGUID = function(guid, callback) {
+	v.player.getValue(guid, "inventory", callback);
 };
 
 // Sets the container GUID of a player
